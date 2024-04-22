6ae3d972b88c9a3c5ad15f86dbcdd50daf535a8b
Jay_local <go.jhson@gmail.com>
2024-04-23 00:04:31 +0900

    Made basic functionality but need some additional parts to have more accurate result

--- cmd/root.go
+++ cmd/root.go
@@ -3,19 +3,101 @@ package cmd
 import (
 	"fmt"
 	"os"
+	"strings"
 
 	"github.com/spf13/cobra"
 )
 
+var (
+	count bool
+	lines bool
+	words bool
+)
+
 var rootCmd = &cobra.Command{
-	Use:   "hugo",
-	Short: "Hugo is a very fast static site generator",
-	Long: `A Fast and Flexible Static Site Generator built with
-				  love by spf13 and friends in Go.
-				  Complete documentation is available at http://hugo.spf13.com`,
+	Use:   "cc",
+	Short: "This is a Word Count App",
 	Run: func(cmd *cobra.Command, args []string) {
-		// Do Stuff Here
+		if len(args) < 1 {
+			panic("Needs to have at lest one argument file path")
+		}
+		fp := args[0]
+
+		if count {
+			getCount(fp)
+		}
+		if lines {
+			getLines(fp)
+		}
+		if words {
+			getWords(fp)
+		}
+
+		if !count && !lines && !words {
+			getCount(fp)
+			getLines(fp)
+			getWords(fp)
+		}
+
+		fmt.Println(fp)
 	},
+}
+
+func init() {
+	rootCmd.PersistentFlags().BoolVarP(&count, "count", "c", false, "Get byte count")
+	rootCmd.PersistentFlags().BoolVarP(&lines, "lines", "l", false, "Get number of lines")
+	rootCmd.PersistentFlags().BoolVarP(&words, "words", "w", false, "Get word count")
+
+}
+
+func getCount(fp string) {
+	b, err := os.ReadFile(fp)
+	if err != nil {
+		fmt.Printf("Cannot read file %s. Please check file path\n", fp)
+		return
+	}
+	fmt.Printf("%d ", len(b))
+}
+
+func getLines(fp string) {
+	b, err := os.ReadFile(fp)
+	if err != nil {
+		fmt.Printf("Cannot read file %s. Please check file path\n", fp)
+		return
+	}
+
+	var line int
+
+	for _, b := range b {
+		if string(b) == "\n" {
+			line++
+		}
+	}
+	fmt.Printf("%d ", line+1)
+}
+
+func getWords(fp string) {
+	b, err := os.ReadFile(fp)
+	if err != nil {
+		fmt.Printf("Cannot read file %s. Please check file path\n", fp)
+		return
+	}
+
+	var word int
+
+	for _, b := range b {
+		//TODO: Do not count when the first character is new line or space
+		//TODO: Do not count when there are multiple new line or space in one row
+
+		// if word == 0 && strings.Contains("\n., ", string(b)) {
+		// 	fmt.Println("first is bla")
+		// 	continue
+		// }
+		if strings.Contains("\n., ", string(b)) {
+			word++
+		}
+	}
+	fmt.Printf("%d ", word+1)
 }
 
 func Execute() {
--- main.go
+++ main.go
@@ -1,23 +1,13 @@
 package main
 
 import (
-	"fmt"
-	"os"
-
 	"github.com/gogojson/wc/cmd"
 )
 
 func main() {
-	const wc = 342190
-	const fileName = "/Users/jayson/Bucket/wc/text_maker/text.txt"
+	// const wc = 342190
+	// const fileName = "/Users/jayson/Bucket/wc/text_maker/text.txt"
 
 	cmd.Execute()
-
-	// Read given file
-	b, err := os.ReadFile(fileName)
-	if err != nil {
-		panic("Cannot read file ")
-	}
-	fmt.Println(len(b))
 
 }
